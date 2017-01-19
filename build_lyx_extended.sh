6eb7820d81bf4aa3b94bbadd226a7259ed9384d3
Christian Ridderström <chr@lyx.org>
2017-01-20 00:01:00 +0100

    Add build script that does additional tasks

---
+++ build_lyx_extended.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+cd "$1"
+
+function failed() {
+    printf "Command '%s' failed, exiting\n" "$*"
+    exit -1
+}
+
+function do_or_fail() {
+    printf "# Executing: %s\n" "$*"
+    "$@" || failed "$@"
+}
+
+
+# git reset --hard && git clean -xdf && 
+do_or_fail ./autogen.sh
+do_or_fail ./configure
+do_or_fail make
+do_or_fail make check
+do_or_fail make distcheck
+echo "GOOD"
